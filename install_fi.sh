5c2b324636261633ff26cee634df30eddc27e2af
ahronmoshe <ahronmoshe1@gmail.com>
2019-12-31 17:58:46 +0200

    Update install_fi.sh

--- install_fi.sh
+++ install_fi.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/ahronmoshe/hack-assistance/master/tools -o /root/tools.txt
+wget https://raw.githubusercontent.com/ahronmoshe/hack-assistance/master/tools -O /root/tools.txt
 mkdir /root/tools
 cd /root/tools
 for i in $(cat /root/tools.txt)
 do
 	git clone $i
 done
-wget https://raw.githubusercontent.com/ahronmoshe/hack-assistance/master/dpkgkali -o /root/dpkgkali.txt
+echo "****************************************";
+echo "finish to download the tools from Github"; 
+echo "****************************************";
+wget https://raw.githubusercontent.com/ahronmoshe/hack-assistance/master/dpkgkali -O /root/dpkgkali.txt
 for i in $(cat /root/dpkgkali.txt)
 do 
 	apt -y install $i
